b48e7daf80c7d4dd69cc0f15a3f6cbd8f83ed8f2
Juan Riccetto <juanmartinre@gmail.com>
2024-06-04 15:24:40 -0300

    jaja salu2

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from scipy.stats import binom
+from scipy.stats import binom, mode
 import numpy as np
 import matplotlib.pyplot as plt
 
 n = 100
 p = 0.35
-sizes = [102, 103, 104, 105]
-
+sizes = [10**2, 10**3, 10**4, 10**5]
 
 def generate_random_samples():
     samples = {}
     for size in sizes:
         samples[size] = binom.rvs(n, p, size=size)
-
+    return samples
 
 def generate_box_diagram(samples):
     fig, axes = plt.subplots(2, 2, figsize=(10, 8))
@@ -24,7 +23,6 @@ def generate_box_diagram(samples):
     plt.tight_layout()
     plt.show()
 
-
 def generate_histogram(samples):
     fig, axes = plt.subplots(2, 2, figsize=(10, 8))
     axes = axes.flatten()
@@ -36,10 +34,20 @@ def generate_histogram(samples):
     plt.tight_layout()
     plt.show()
 
+def calculate_statistics(samples):
+    for size in sizes:
+        sample = samples[size]
+        print(f'Para la muestra de tamaño {size}:')
+        print(f' - Mediana: {np.median(sample)}')
+        print(f' - Moda: {mode(sample)[0][0]}')
+        print(f' - Media empírica: {np.mean(sample)}')
+        print(f' - Varianza empírica: {np.var(sample)}')
 
 def main():
-    generate_random_samples()
+    samples = generate_random_samples()
+    generate_box_diagram(samples)
+    generate_histogram(samples)
+    calculate_statistics(samples)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
