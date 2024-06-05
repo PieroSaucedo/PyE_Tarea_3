ae6480ffa4e98c94c0ac8df87b3227f5b51a581e
Juan Riccetto <juanmartinre@gmail.com>
2024-06-05 19:23:36 -0300

    esperemos que funcione

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
-from scipy.stats import binom, mode
+from scipy.stats import binom, geom, poisson, mode
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 100
-p = 0.35
 sizes = [10**2, 10**3, 10**4, 10**5]
 
-def generate_random_samples():
+#Datos para Distribución Binomial.
+n = 100
+p = 0.35
+
+# Datos para Distribución Geométrica.
+pgeom = 0.08
+
+#Datos para Distribución de Poisson.
+L = 30
+
+#Genera muestras de distribución binomial.
+def generate_random_samples_binom():
     samples = {}
     for size in sizes:
         samples[size] = binom.rvs(n, p, size=size)
     return samples
 
+#Genera muestras de distribución geométrica.
+def generate_random_samples_geom():
+    samples = {}
+    for size in sizes:
+        samples[size] = geom.rvs(pgeom, size=size)
+    return samples
+#Genera muestras de distribución de Poisson.
+def generate_random_samples_poisson():
+    samples = {}
+    for size in sizes:
+        samples[size] = poisson.rvs(L, size=size)
+    return samples
+
+#Genera diagrama de cajas a partir de las muestras
 def generate_box_diagram(samples):
     fig, axes = plt.subplots(2, 2, figsize=(10, 8))
     axes = axes.flatten()
@@ -23,6 +46,7 @@ def generate_box_diagram(samples):
     plt.tight_layout()
     plt.show()
 
+#Genera histograma a partir de las muestras
 def generate_histogram(samples):
     fig, axes = plt.subplots(2, 2, figsize=(10, 8))
     axes = axes.flatten()
@@ -34,20 +58,34 @@ def generate_histogram(samples):
     plt.tight_layout()
     plt.show()
 
+#Calcula y muestra en pantalla diferentes estadísticas
 def calculate_statistics(samples):
     for size in sizes:
         sample = samples[size]
         print(f'Para la muestra de tamaño {size}:')
         print(f' - Mediana: {np.median(sample)}')
-        print(f' - Moda: {mode(sample)[0][0]}')
+        print(f' - Moda: {mode(sample)}')
         print(f' - Media empírica: {np.mean(sample)}')
         print(f' - Varianza empírica: {np.var(sample)}')
 
 def main():
-    samples = generate_random_samples()
-    generate_box_diagram(samples)
-    generate_histogram(samples)
-    calculate_statistics(samples)
-
+    #Ejercicio 1
+    samples1 = generate_random_samples_binom()
+    generate_box_diagram(samples1)
+    generate_histogram(samples1)
+    calculate_statistics(samples1)
+    
+    #Ejercicio 2
+    samples2 = generate_random_samples_geom()
+    generate_box_diagram(samples2)
+    generate_histogram(samples2)
+    calculate_statistics(samples2)
+    
+    #Ejercicio 3
+    samples3 = generate_random_samples_poisson()
+    generate_box_diagram(samples3)
+    generate_histogram(samples3)
+    calculate_statistics(samples3)
+    
 if __name__ == "__main__":
     main()
