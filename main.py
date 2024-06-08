6f81d33976e7267d94bfc4a82b220821ac5ff7dd
ikken <pierosaucedo3@gmail.com>
2024-06-08 13:59:11 -0300

    xd

--- main.py
+++ main.py
@@ -40,26 +40,30 @@ def generate_random_samples_poisson():
 
 
 # Genera un diagrama de cajas a partir de las muestras obtenidas.
-def generate_box_diagram(samples):
+def generate_box_diagram(samples, distribution_type):
     fig, axes = plt.subplots(2, 2, figsize=(10, 8))
     axes = axes.flatten()
 
     for i, size in enumerate(sizes):
         axes[i].boxplot(samples[size])
-        axes[i].set_title(f'Tamaño de muestra: {size}')
+        axes[i].set_title(
+            f'Distribución {distribution_type} con tamaño = {size}'
+        )
 
     plt.tight_layout()
     plt.show()
 
 
 # Genera un histograma a partir de las muestras obtenidas.
-def generate_histogram(samples):
+def generate_histogram(samples, distribution_type):
     fig, axes = plt.subplots(2, 2, figsize=(10, 8))
     axes = axes.flatten()
 
     for i, size in enumerate(sizes):
         axes[i].hist(samples[size], bins=15)
-        axes[i].set_title(f'Tamaño de muestra: {size}')
+        axes[i].set_title(
+            f'Distribución {distribution_type} con tamaño = {size}'
+        )
 
     plt.tight_layout()
     plt.show()
@@ -72,29 +76,29 @@ def calculate_statistics(samples):
         sample = samples[size]
         print(f'Para la muestra de tamaño {size}:')
         print(f' - Mediana: {np.median(sample)}')
-        print(f' - Moda: {mode(sample)}')
+        print(f' - Moda: {mode(sample)[0]}')
         print(f' - Media empírica: {np.mean(sample)}')
         print(f' - Varianza empírica: {np.var(sample)}')
 
 
 def main():
     # Ejercicio 1
-    samples1 = generate_random_samples_binom()
-    generate_box_diagram(samples1)
-    generate_histogram(samples1)
-    calculate_statistics(samples1)
+    samples_binom = generate_random_samples_binom()
+    generate_box_diagram(samples_binom, 'Binomial')
+    generate_histogram(samples_binom, 'Binomial')
+    calculate_statistics(samples_binom)
     
     # Ejercicio 2
-    samples2 = generate_random_samples_geom()
-    generate_box_diagram(samples2)
-    generate_histogram(samples2)
-    calculate_statistics(samples2)
+    samples_geom = generate_random_samples_geom()
+    generate_box_diagram(samples_geom, 'Geométrica')
+    generate_histogram(samples_geom, 'Geométrica')
+    calculate_statistics(samples_geom)
     
     # Ejercicio 3
-    samples3 = generate_random_samples_poisson()
-    generate_box_diagram(samples3)
-    generate_histogram(samples3)
-    calculate_statistics(samples3)
+    samples_poisson = generate_random_samples_poisson()
+    generate_box_diagram(samples_poisson, 'Poisson')
+    generate_histogram(samples_poisson, 'Poisson')
+    calculate_statistics(samples_poisson)
 
 
 if __name__ == "__main__":
